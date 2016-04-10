64183f49ff2cb384193300a8143cc18c342ec536
Yuqi Hou <yuqi.hou@gmail.com>
2016-04-10 18:26:47 -0400

    Added intro scene dialog, instructions, and input buttons

---
+++ Assets/Scripts/IntroBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Submit") > 0)
+		{
+			Debug.Log ("Submit button detected");
+			SceneManager.LoadScene("instructionScene", LoadSceneMode.Single);
+		}
+	}
+}
---
+++ Assets/Scripts/PortalBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
