de98c3c7add727305fe5a11963e6940998f0ec83
Rachel Thornton <rthorn@mit.edu>
2016-04-13 15:21:27 -0400

    friction

--- Assets/Scripts/PortalBehaviourScript.cs
+++ Assets/Scripts/PortalBehaviourScript.cs
@@ -33,7 +33,7 @@ public class PortalBehaviourScript : MonoBehaviour {
 
 	IEnumerator LoadNextLevel()
 	{
-		yield return new WaitForSeconds(3.0f);
+		yield return new WaitForSeconds(1.5f);
 		nextSceneIndex = sceneIndex + 1;
 		Debug.Log (nextSceneIndex);
 		SceneManager.LoadScene(sceneNames[nextSceneIndex], LoadSceneMode.Single);
