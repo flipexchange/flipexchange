4ff562d661d21a7cc53ecda84a674b2c7555e79c
Dlearn <dylanho92@gmail.com>
2016-04-17 16:23:09 -0400

    Enemy kills you.

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class enemyController : MonoBehaviour
 {
-
+    /*  PATROL LOGIC  */
     protected Vector3 velocity;
     public Transform _transform;
     public float distance = 5f;
@@ -11,6 +11,16 @@ public class enemyController : MonoBehaviour
     Vector3 _originalPosition;
     bool isGoingLeft = true;
     private float distFromStart;
+
+    /*  BULLET LOGIC  */
+    //Drag in the Bullet Emitter from the Component Inspector.
+    public GameObject Bullet_Emitter;
+
+    //Drag in the Bullet Prefab from the Component Inspector.
+    public GameObject Bullet;
+
+    //Enter the Speed of the Bullet from the Component Inspector.
+    public float Bullet_Forward_Force;
 
     public void Start()
     {
@@ -39,6 +49,27 @@ public class enemyController : MonoBehaviour
                 SwitchDirection();
 
             _transform.Translate(velocity.x * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            //The Bullet instantiation happens here.
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Bullet, Bullet_Emitter.transform.position, Bullet_Emitter.transform.rotation) as GameObject;
+
+            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+            //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+            //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force.
+            Temporary_RigidBody.AddForce(transform.forward * Bullet_Forward_Force);
+
+            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+            Destroy(Temporary_Bullet_Handler, 10.0f);
         }
     }
     void SwitchDirection()
