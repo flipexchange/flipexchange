4deb07ebffd73bbc4705317d8b125bd4cf820cd1
Rachel Thornton <rthorn@mit.edu>
2016-04-18 09:54:08 -0400

    fixed circle collider

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -123,10 +123,8 @@ public class PlayerControls : MonoBehaviour {
 			}
 		}
 
-		if (tilted) {
-			if (rb2d.rotation == 0f) {
-				rb2d.AddForce (new Vector2(0f,-10000));
-			} else {
+		if (tilted || !pink) {
+			if (rb2d.rotation != 0f) {
 				rb2d.rotation = 0f;
 			}
 		}
@@ -204,10 +202,10 @@ public class PlayerControls : MonoBehaviour {
         if (col.gameObject.layer == 10) { //int value of 'Death' in layer manager(User Defined starts at 10)
             dead = true;
         }
-		if(col.collider.bounds.Contains(transform.position))
+		/*if(col.collider.bounds.Contains(transform.position))
 		{
 			dead = true;
-		}
+		}*/
 		if (col.transform.gameObject.name == "Kickable") {
 			kick = true;
 			kickee = col.transform.gameObject;
