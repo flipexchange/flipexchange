bf4ac6b0d65d8e70401b2153de465056d56734c9
jennycxu <jennycxu@mit.edu>
2016-05-02 10:39:43 -0400

    Added all dialogue into levels

--- Assets/Scripts/dialogBehaviour.cs
+++ Assets/Scripts/dialogBehaviour.cs
@@ -21,6 +21,7 @@ public class dialogBehaviour : MonoBehaviour {
 	private bool showingQuote;
 	private AudioSource source;
 	public AudioClip textAudio;
+	public int level;
 	// Use this for initialization
 	void Start () {
 		source = GetComponent<AudioSource> ();
@@ -40,33 +41,81 @@ public class dialogBehaviour : MonoBehaviour {
 			X2 = 2;
 			Y1 = 1;
 			Y2 = 3;
-			//[x1,y1,x2,y2] where x1,y1 is initial position and x2,y2 is where it goes after it teleports
-			infoPositions = new float[,] {
-				{ -5, -6, 5, -6 },
-				{ 9, -4, 17, -4 },
-				{ 47.86f, -4, 67, -4 },
-				{ 78, -2.89f, 83, -2.89f },
-				 {
-					100,
-					-5,
-					199,
-					-5
-				}
-			};
-
 			rectQuotes = new string[5];
 			circleQuotes = new string[5];
-			rectQuotes [0] = "I bet I can get to the end of this world all on my own! Why do we need to be merged?";
-			circleQuotes [0] = "You fire folk are so brash. You always rush things; I can't wait until we unmerge.";
+			//[x1,y1,x2,y2] where x1,y1 is initial position and x2,y2 is where it goes after it teleports
+			if (level == 1) {
+				infoPositions = new float[,] {
+					{ -5, -6, 5, -6 },
+					{ 9, -4, 17, -4 },
+					{ 47.86f, -4, 67, -4 },
+					{ 78, -2.89f, 83, -2.89f }, {
+						100,
+						-5,
+						199,
+						-5
+					}
+				};
 
-			circleQuotes [1] = "Even if you can jump and run faster than me, that doesn't mean you can climb ramps as well as I can...";
-			rectQuotes [1] = "Ice Maiden, this ramp is too steep even for me! I can't seem to rush past it...but maybe you can?";
-			rectQuotes [2] = "What are those weird red bars? Why can't I get past them on my own?";
-			circleQuotes [2] = "Hm, the red bars don't seem to block me. Let me take over for a little while.";
-			rectQuotes [3] = "Man, I wish I could jump high enough to reach that white portal...but we might need to switch off to make it.";
-			circleQuotes [3] = "Seems like there is no way for us to make it to the portal unless we switch off.";
+
+				rectQuotes [0] = "I bet I can get to the end of this world all on my own! Why do we need to be merged?";
+				circleQuotes [0] = "You fire folk are so brash. You always rush things; I can't wait until we unmerge.";
+
+				circleQuotes [1] = "Even if you can jump and run faster than me, that doesn't mean you can climb ramps as well as I can...";
+				rectQuotes [1] = "Ice Maiden, this ramp is too steep even for me! I can't seem to rush past it...but maybe you can?";
+				rectQuotes [2] = "What are those weird red bars? Why can't I get past them on my own?";
+				circleQuotes [2] = "Hm, the red bars don't seem to block me. Let me take over for a little while.";
+				rectQuotes [3] = "Man, I wish I could jump high enough to reach that white portal...but we might need to switch off to make it.";
+				circleQuotes [3] = "Seems like there is no way for us to make it to the portal unless we switch off.";
+			} else if (level == 2) {
+				infoPositions = new float[,] {
+					{ 1, -4.5f, 8.5f, -4 },
+					{ 16, -4.5f, 42, -0.26f },
+					{ 55, -0.43f, 72, 0 },
+					{ 87, -0.43f, 113, -3f }, {
+						150,
+						-5,
+						199,
+						-5
+					}
+				};
 
 
+				rectQuotes [0] = "What's this rectangular thing and why can't I push it over myself?";
+				circleQuotes [0] = "You're way too light to get that over. We ice folk are much more competent at this type of thing.";
+
+				circleQuotes [1] = "Pay attention! That rock's going to kill us if we don't find a way to switch off and avoid it.";
+				rectQuotes [1] = "I don't think I can jump high enough to get over that rock...but I can still try?";
+				rectQuotes [2] = "We might have gotten past that rock but just sayin', I did most of the work there.";
+				circleQuotes [2] = "I definitely helped us get under that platform so I'd say that I did more of the work there...";
+				rectQuotes [3] = "Eek seems like someone's not happy about us being here together, let's try to somehow hide from the bullets?";
+				circleQuotes [3] = "That guy really doesn't want the Fire and Ice nations to merge. But we have to get past him.";
+
+			} else  {
+				infoPositions = new float[,] {
+					{ 0.56f, -6.2f, 18.5f, -2 },
+					{ 34, -3.2f, 40, -0.55f },
+					{ 43, -0.55f, 45.72f, -2.1f },
+					{ 76, -6.43f, 79.9f, -6.4f }, {
+						150,
+						-5,
+						199,
+						-5
+					}
+				};
+
+
+				rectQuotes [0] = "Is this the end? I've think I've gotten the hang of this by now.";
+				circleQuotes [0] = "Come on. Let's not slack off now. There's still one last obstacle for us.";
+
+				circleQuotes [1] = "There's no way for us to beat this monster unless we find a way to push those rocks on his head.";
+				rectQuotes [1] = "Jeez this guy won't stop shooting bullets, I think we might be able to jump above him if we switch off?";
+				rectQuotes [2] = "Time to put an end to this guy.";
+				circleQuotes [2] = "This rock will probably hurt him and slow him down.";
+				rectQuotes [3] = "So we did it. I can't believe we really did it. You aren't half bad.";
+				circleQuotes [3] = "That was quite the journey. Truthfully, I don't know if  I could've done it without you.";
+
+			}
 			//setting things in place
 			Vector3 newPos = new Vector3 (infoPositions [counter, X1], infoPositions [counter, Y1], infoBox.transform.position.z);
 			infoBox.transform.position = newPos;
