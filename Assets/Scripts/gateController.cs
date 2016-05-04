14a314cc83a11875aa2409c6472c53dff5d3f2e5
Yuqi Hou <yuqi.hou@gmail.com>
2016-05-04 16:11:10 -0400

    Added Key and Gate Behaviour

---
+++ Assets/Scripts/gateController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gateController : MonoBehaviour {
+
+	public GameObject Key;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(coll.GetComponent<CircleCollider2D>().gameObject==Key){
+			
+			Debug.Log ("Collision detected");
+			StartCoroutine("OpenGate");
+			Destroy(Key);
+		}
+	}
+
+	IEnumerator OpenGate()
+	{
+		// Move the Gate object by y
+		var endPosition = gameObject.transform.position.y+2;
+		while (gameObject.transform.position.y< endPosition) {
+			Vector3 newPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y+0.05f, gameObject.transform.position.z);
+			gameObject.transform.position = newPosition;
+			yield return new WaitForSeconds (0.03f);
+		}
+	}
+
+
+}
