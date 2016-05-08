09ffd5efc8f91ea57f41ba6327dd7bc27bf4a5e7
jennycxu <jennycxu@mit.edu>
2016-05-08 11:50:30 -0400

    Added new boss music

--- Assets/Scripts/gateController.cs
+++ Assets/Scripts/gateController.cs
@@ -6,22 +6,39 @@ public class gateController : MonoBehaviour {
 	public GameObject Key;
 	private AudioSource deathAudioSource;
 	public AudioClip deathAudio;
+	private AudioSource prebattleAudioSource;
+	public AudioClip prebattleAudio;
+	public AudioClip battleAudio;
+	private GameObject player;
+	private bool playingBossSound;
 
 	// Use this for initialization
 	void Awake(){
 		deathAudioSource = GetComponent<AudioSource> ();
 		deathAudioSource.clip = deathAudio;
+		prebattleAudioSource = gameObject.AddComponent<AudioSource>();
+		prebattleAudioSource.clip = prebattleAudio;
+		prebattleAudioSource.loop = true;
 	} 
 	void Start () {
-		
+		prebattleAudioSource.Play ();
+		player = GameObject.Find ("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (player.transform.position.x > 25) {
+			if (playingBossSound == false) {
+				playingBossSound = true;
+				prebattleAudioSource.clip = battleAudio;
+				prebattleAudioSource.Play ();
+			}
+		}
 	}
 	public void bossDeath()
 	{
+		prebattleAudioSource.clip = prebattleAudio;
+		prebattleAudioSource.Play ();
 		deathAudioSource.Play ();
 	}
 	void OnCollisionEnter2D(Collision2D coll)
