7d0546d07086eb86ca0dd6f7a0e58f3a78d88b93
Erica Yuen <ejyuen@mit.edu>
2016-05-11 02:48:46 -0400

    ice box colliders

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Camera : MonoBehaviour {
 
@@ -11,7 +12,12 @@ public class Camera : MonoBehaviour {
 
 	void Update() {
 		var diff = target.position.x-transform.position.x;
-		transform.position = new Vector3(target.position.x+5,transform.position.y,transform.position.z);
+		if (!(SceneManager.GetActiveScene ().name == "ThirdLevel")) {
+			transform.position = new Vector3 (target.position.x + 5, transform.position.y, transform.position.z);
+		} else {
+			transform.position = new Vector3 (target.position.x, transform.position.y, transform.position.z);
+		}
+
 		if (move != null) {
 			move.transform.position = new Vector3 (move.transform.position.x + diff, move.transform.position.y, move.transform.position.z);
 		}
